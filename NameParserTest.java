5c4559ca4d7666cd78a4cfd7fa59b7c011e76c0f
voicehovski <help.kiev@gmail.com>
2022-05-26 16:44:33 +0300

    Create NameParserTest.java

---
+++ NameParserTest.java
@@ -0,0 +1,25 @@
+class NameParser {
+    public String parse(String[] names) {
+        StringBuilder result = new StringBuilder ( "" );
+
+        for(int i = 0; i < names.length; i++) {
+            String firstName = names[i].split(" ")[0];
+
+            result .append ( firstName );
+            if (i != names.length - 1) {
+                result .append ( ", " );
+            }
+        }
+
+        return result .toString (  );
+    }
+}
+
+class NameParserTest {
+    public static void main(String[] args) {
+        String[] names = {"John Doe", "Bill Mask", "Nigua Joshua"};
+
+        //John, Bill, Nigua
+        System.out.println(new NameParser().parse(names));
+    }
+}
